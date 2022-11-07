46158fe9e5f0440660faeb76967a77d1f3107629
mattdev95 <matthewedwards100@yahoo.com>
2022-11-07 22:17:03 +0000

    Remove unused imports from Main class

--- src/main/java/com/mattdev95/Main.java
+++ src/main/java/com/mattdev95/Main.java
@@ -1,12 +1,8 @@
 package com.mattdev95;
 import com.mattdev95.anagram.AnagramChecker;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         var anagramChecker = new AnagramChecker();
         anagramChecker.checkAnagramInput();
     }
